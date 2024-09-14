d626684428dd43b4083fd70069d394f06cd58d3c
chen <chentiantian07@gmail.com>
2024-09-14 22:14:08 +0800

    Revert "Merge pull request #9 from undefinedWu/release_09_14"
    This reverts commit 00de10e472196e9c4e6a80eb801b1557e52b0c84, reversing
    changes made to 8b9769403895cf24c213e7a757317fa717ed1457.

--- index.ts
+++ index.ts
@@ -3,6 +3,3 @@ console.log("revert four add content revert_02 add some new contents")
 
 
 console.log("add new branch for merging request") // revert 3 wants produce the conflict
-
-
-console.log("feature_01 add content")
