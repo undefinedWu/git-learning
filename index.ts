b45dea8535de0b56342703b6bf1ecddb2b4e41f9
chen <chentiantian07@gmail.com>
2024-09-15 00:03:41 +0800

    fix: chenwu_01 update its content

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ console.log("revert one add one new commit")
 console.log("revert four add content revert_02 add some new contents")
 
 
-console.log("add new branch for merging request") // revert 3 wants produce the conflict
+console.log("add new branch for merging request chenwu 01 修改了") // revert 3 wants produce the conflict
 
 
 console.log("feature_01 add content")
