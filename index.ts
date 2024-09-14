9e8bc10e6eda797a2590e8ac17ac1f1201950e5e
chen <chentiantian07@gmail.com>
2024-09-15 00:19:16 +0800

    chore: update the bug

--- index.ts
+++ index.ts
@@ -3,5 +3,7 @@ console.log("revert four add content revert_02 add some new contents")
 
 console.log("add new branch for merging request chenwu 01 修改了") // revert 3 wants produce the conflict chenwu_02 修改
 
+console.log("revert 后 chenwu02 添加了一些东西的")
+
 
 console.log("feature_01 add content")
